78a43e5549f2bd6491a35e4ec33af0dbe5de15d3
freedomlang <blackywkl@gmail.com>
2019-09-05 21:38:03 +0800

    feat: Add 404 page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import ArticleList from './components/ArticleList';
 import ArticleDetail from './components/ArticleDetail';
+import NotFoundPage from './pages/404'
 import {
   HashRouter as Router,
   Route,
   Redirect,
-  Link
+  Switch
 } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
@@ -19,9 +20,12 @@ class App extends Component {
         <div className="App">
           <Header />
 
-          <Route exact path="/" component={ArticleList} />
-          <Route exact path="/article/:id" component={ArticleDetail} />
-
+          <Switch>
+            <Route exact path="/" component={ArticleList} />
+            <Route exact path="/article/:id" component={ArticleDetail} />
+            <Route exact path="/404" component={NotFoundPage} />
+            <Redirect to="/404" />
+          </Switch>
           <Footer />
         </div>
       </Router>
