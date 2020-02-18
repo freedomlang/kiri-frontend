5753d110751a07ee932723fdebdc53d6c69f2057
freedomlang <blackywkl@gmail.com>
2020-02-18 11:28:47 +0800

    chore: Update indention to 2 spaces

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,25 +1,21 @@
-import React, { memo } from 'react'
-import './style.scss';
+import React, { memo } from "react";
+import "./style.scss";
 
-function About () {
-    return (
-        <div className="aboutMe">
-            <p>about me</p>
-            <div className="row">
-                <div className="col-4">
-                    <h3 className="section-title">
-                        关于我
-                    </h3>
-                    <p className="mt-2">
-                        一枚前端开发工程狮
-                    </p>
-                </div>
-                <div className="col-8">
-                    这不是一个完完全全的技术博客，只是一个前端开发攻城狮的自留地；所以除了技术文章之外，这里会有我的一些吐槽和生活记录。
-                </div>
-            </div>
+function About() {
+  return (
+    <div className="aboutMe">
+      <p>about me</p>
+      <div className="row">
+        <div className="col-4">
+          <h3 className="section-title">关于我</h3>
+          <p className="mt-2">一枚前端开发工程狮</p>
         </div>
-    )
+        <div className="col-8">
+          这不是一个完完全全的技术博客，只是一个前端开发攻城狮的自留地；所以除了技术文章之外，这里会有我的一些吐槽和生活记录。
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default memo(About);
