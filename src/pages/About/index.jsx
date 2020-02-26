ed0d724901fe87b82a97d9811ce3a2e693e1d642
freedomlang <blackywkl@gmail.com>
2020-02-26 09:36:26 +0800

    feat(about): Add animation for about panel

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -3,12 +3,12 @@ import "./style.scss";
 
 function About() {
   return (
-    <div className="aboutMe">
+    <div className="aboutMe animated faster fadeInUp">
       <p>about me</p>
       <div className="row">
         <div className="col-4">
           <h3 className="section-title">关于我</h3>
-          <p className="mt-2">一枚前端开发工程狮</p>
+          <p className="mt-2">一枚前端开发攻城狮</p>
         </div>
         <div className="col-8">
           这不是一个完完全全的技术博客，只是一个前端开发攻城狮的自留地；所以除了技术文章之外，这里会有我的一些吐槽和生活记录。
