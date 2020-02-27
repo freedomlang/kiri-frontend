ce5624adf71c5cf680ded14ee681cea096074c08
freedomlang <blackywkl@gmail.com>
2020-02-27 15:27:46 +0800

    chore: Update workbox config

--- config-overrides.js
+++ config-overrides.js
@@ -2,10 +2,18 @@ const path = require('path');
 const {
   override,
   fixBabelImports,
+  adjustWorkbox,
   addWebpackAlias
 } = require("customize-cra");
 
 module.exports = override(
+  adjustWorkbox(wb =>
+    Object.assign(wb, {
+      skipWaiting: true,
+      navigateFallbackBlacklist: [new RegExp('^/dieci')]
+      // navigateFallbackDenylist: [new RegExp('^/dieci')]
+    })
+  ),
   addWebpackAlias({
     images: path.resolve(__dirname, "src/assets/images/"),
     '@ant-design/icons/lib/dist$': path.join(__dirname, 'scripts/icons.js')
