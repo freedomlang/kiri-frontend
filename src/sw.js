49accd6f1a2f5bb9dbb8505fe85b2e67b57f0bda
freedomlang <blackywkl@gmail.com>
2020-02-28 10:11:31 +0800

    chore: Cache assets from cdn

--- src/sw.js
+++ src/sw.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-restricted-globals */
 self.addEventListener("message", event => {
   if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
@@ -9,24 +11,26 @@ workbox.core.clientsClaim();
 
 // cache name
 workbox.core.setCacheNameDetails({
-  prefix: "My-blog-cache-images",
+  prefix: "My-blog-cache",
   precache: "precache",
-  runtime: "runtime"
+  runtime: "run-time"
 });
 
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-
-const {registerRoute} = workbox.routing;
-const {CacheFirst} = workbox.strategies;
+const { registerRoute } = workbox.routing;
+const { CacheFirst } = workbox.strategies;
+const { Plugin: CacheableResponsePlugin } = workbox.cacheableResponse;
 
 registerRoute(
   /\.(?:png|jpg|jpeg|svg|gif)$/,
   new CacheFirst({
-    cacheName: 'My-blog-cache-images',
-    plugins: [
-      new workbox.cacheableResponse.Plugin({statuses: [0, 200]})
-    ],
+    plugins: [new CacheableResponsePlugin({ statuses: [0, 200] })]
   })
 );
+
+registerRoute(
+  /cdn\.bootcss\.com/,
+  new CacheFirst({})
+)
